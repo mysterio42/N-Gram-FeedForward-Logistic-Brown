4fe83341c43f719f1e67c0c33cd42da66b9cec3c
mysterio42 <bachana3435@gmail.com>
2020-05-15 00:19:38 +0400

    change run for model

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from models.embedding import OneHotEmbedding
-from models.net import FeedForward
+from models.net import FeedForward,FeedForward_Hidden
 
 np.set_printoptions(edgeitems=50)
 np.random.seed(1)
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     emb = OneHotEmbedding(200, 0.1)
     emb.build()
 
-    model = FeedForward(embedding=emb)
-    model.train(epochs=1, n_gram=3, lr=0.01)
+    # model = FeedForward(embedding=emb)
+    model = FeedForward_Hidden(embedding=emb,hidden_dim=100)
+    model.train(epochs=1, n_gram=2)
